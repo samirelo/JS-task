741c9f3d709e8d2a030f5d316d4a8a5ce2c52b97
samirelo <samir@qburst.com>
2018-01-23 15:27:11 +0530

    Slider - Non responsive

--- scripts.js
+++ scripts.js
@@ -1,1 +1,15 @@
 console.log('hahah');
+
+sliderCount = 1;
+
+function changeSliderImage(sliderDir) {
+  if(sliderDir=="left")
+    sliderCount -= 1;
+  else
+    sliderCount += 1;
+  if (sliderCount>7 && sliderDir=="right")
+    sliderCount = 1;
+  else if (sliderCount<1 && sliderDir=="left")
+    sliderCount = 7;
+  document.getElementById('myimage').src = "./images/image_"+String(sliderCount)+".jpg";
+}
