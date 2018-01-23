662f4f849cdd85a708b0987bcf18e4d9f37ca384
Sachin <sachink@qburst.com>
2018-01-23 14:24:32 +0530

    Indentation corrected

--- scripts.js
+++ scripts.js
@@ -1,37 +1,44 @@
-console.log('hahah');
 var table;
 
 function getData() {
   fetch('https://jsonplaceholder.typicode.com/users')
-        .then((res) => res.json())
-        .then((data) => {
-         data.forEach((user) => {
-  table = document.getElementById('table');
-  var row = table.insertRow(-1);
-  var cell_id = row.insertCell(0);
-	var cell_name = row.insertCell(1);
-	var cell_username = row.insertCell(2);
-  var cell_email = row.insertCell(3);
-	cell_id.innerHTML = user.id;
-	cell_name.innerHTML = user.name;
-	cell_username.innerHTML = user.username;
-	cell_email.innerHTML = user.email;
-        });
-  })
+    .then((res) => res.json())
+    .then((data) => {
+      data.forEach((user) => {
+        table = document.getElementById('table');
+        var row = table.insertRow(-1);
+        var cell_id = row.insertCell(0);
+        var cell_name = row.insertCell(1);
+        var cell_username = row.insertCell(2);
+        var cell_email = row.insertCell(3);
+        cell_id.innerHTML = user.id;
+        cell_name.innerHTML = user.name;
+        cell_username.innerHTML = user.username;
+        cell_email.innerHTML = user.email;
+      });
+    })
 }
 
 function sortTable() {
   table = document.getElementById("table");
-  var direction, isSwitching, toSwitch, x, y, count = 0, i;
+  var direction, isSwitching, toSwitch, x, y, count = 0,
+    i;
+  // Set the sorting direction
   direction = "asc";
   isSwitching = true;
   while (isSwitching) {
+    /* isSwitching determines whether any switching is being done.
+    Initially assume that no switching has to be done */
     isSwitching = false;
     rows = table.getElementsByTagName("TR");
-    for(i = 1; i < (rows.length - 1); i++) {
+    // Loop through all row elements and compare successive ones
+    for (i = 1; i < (rows.length - 1); i++) {
+      /* toSwitch determines whether elements should be switched or not.
+      Initially assume that no switching is to be made*/
       toSwitch = false;
       x = rows[i].getElementsByTagName("TD")[1];
       y = rows[i + 1].getElementsByTagName("TD")[1];
+      // Check whether to switch rows based on direction
       if (direction == "asc") {
         if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
           toSwitch = true
@@ -39,19 +46,23 @@ function sortTable() {
         }
       } else if (direction == "desc") {
         if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
-        toSwitch = true;
-        break;
+          toSwitch = true;
+          break;
         }
       }
     }
+    // If toSwitch has been set, then swap the rows
     if (toSwitch) {
       rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
       isSwitching = true;
-      count ++;
+      // count is incremented each time a switch has been made
+      count++;
     } else {
+      /* If count is 0 and direction is 'asc', run the loop again by
+      changing direction to 'desc' */
       if (count == 0 && direction == "asc") {
-      direction = "desc";
-      isSwitching = true;
+        direction = "desc";
+        isSwitching = true;
       }
     }
   }
